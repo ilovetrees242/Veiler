87e67a9f54d743ef7ed094bb7825b9d58db7aab5
ilovetrees242 <riyrty594@gmail.com>
2026-01-25 17:07:09 +0530

    Updated Veiler script

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@ if [ $(id -u) -ne 0 ]; then
     echo "Please run as root."
     exit 1
 fi
-install -v -m 771 Veiler /usr/bin/
+install -v -m 777 ./Veiler /usr/bin/
 install -v -m 660 -d /usr/lib/Veiler/Tools
+install -v -m 660 -d /var/db/Veiler
+touch /var/db/Veiler/package.db
+install -v -d 660 /var/cache/Veiler
+chmod 660 /var/db/Veiler/package.db
 cp -ra Tools/* /usr/lib/Veiler/Tools
