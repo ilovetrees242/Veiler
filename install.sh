5d39160aaa2809afccb68f88adb440082325869b
ilovetrees242 <riyrty594@gmail.com>
2026-03-15 19:53:10 +0530

    Cleaned up unnecessary stuff and restructured database like arch's

--- install.sh
+++ install.sh
@@ -2,10 +2,14 @@ if [ $(id -u) -ne 0 ]; then
     echo "Run as root."
     exit 1
 fi
-install -v -m 777 ./Veiler /usr/bin/
+set -e
+trap 'echo "Error while installing"' INT ERR TERM
+install -v -m 770 ./Veiler /usr/bin/
 install -v -m 660 -d /usr/lib/Veiler/Tools
 install -v -m 660 -d /var/db/Veiler/
-install -v -m 660 ./package.db /var/db/Veiler/
+install -v -m 660 -d /var/db/Veiler/local
+tar -C /var/db/Veiler/ -xvf db.tar.gz
 install -v -m 660 -d /var/cache/Veiler
 install -v -m 660 veiler.conf /etc/
 cp -ra Tools/* /usr/lib/Veiler/Tools
+set +e
