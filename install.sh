fba3dc81efa662cf6da7e729d1eeb8af015f3205
ilovetrees242 <riyrty594@gmail.com>
2026-03-17 12:46:35 +0530

    Changes in permissions, new config option

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ fi
 set -e
 trap 'echo "Error while installing"' INT ERR TERM
 install -v -m 770 ./Veiler /usr/bin/
-install -v -m 660 -d /usr/lib/Veiler/Tools
-install -v -m 660 -d /var/db/Veiler/
-install -v -m 660 -d /var/db/Veiler/local
-tar -C /var/db/Veiler/ -xvf db.tar.gz
-install -v -m 660 -d /var/cache/Veiler
-install -v -m 660 veiler.conf /etc/
+install -v -m 550 -d /usr/lib/Veiler/Tools
+install -v -m 550 -d /var/db/Veiler/
+install -v -m 550 -d /var/db/Veiler/local
+tar --strip-components=1 -C /var/db/Veiler/ -xvf db.tar.gz
+install -v -m 550 -d /var/cache/Veiler
+install -v -m 774 veiler.conf /etc/
 cp -ra Tools/* /usr/lib/Veiler/Tools
 set +e
