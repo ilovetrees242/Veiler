e4025c9d47ff7a15545aadcbb99fc6dd5bb85858
ilovetrees242 <riyrty594@gmail.com>
2026-03-27 00:34:03 +0530

    Changes

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ if [ $(id -u) -ne 0 ]; then
 fi
 set -e
 trap 'echo "Error while installing"' INT ERR TERM
-install -v -m 770 ./Veiler /usr/bin/
-install -v -m 550 -d /var/db/Veiler/
-install -v -m 550 -d /var/db/Veiler/local
+install -v -m 774 ./Veiler /usr/bin/
+install -v -m 775 -d /var/db/Veiler/
+install -v -m 775 -d /var/db/Veiler/local
 tar --strip-components=1 -C /var/db/Veiler/ -xvf db.tar.gz
-install -v -m 550 -d /var/cache/Veiler
+install -v -m 554 -d /var/cache/Veiler
 install -v -m 774 veiler.conf /etc/
 set +e
