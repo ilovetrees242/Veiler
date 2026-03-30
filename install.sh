51e35f9a7e3ed0af00fc1fa34067c9f138b7e47d
ilovetrees242 <riyrty594@gmail.com>
2026-03-30 23:55:04 +0530

    New packages, and fixes

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ set -e
 trap 'echo "Error while installing"' INT ERR TERM
 install -v -m 775 ./Veiler /usr/bin/
 install -v -m 775 -d /var/db/Veiler/
+install -v -m 775 -d /usr/lib/Veiler
 install -v -m 775 -d /var/db/Veiler/local
 tar --strip-components=1 -C /var/db/Veiler/ -xvf db.tar.gz
 install -v -m 774 -d /var/cache/Veiler
