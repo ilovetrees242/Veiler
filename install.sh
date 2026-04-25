c4256c538e270e244e4744694885df3cd350223e
ilovetrees242 <riyrty594@gmail.com>
2026-04-26 00:25:36 +0530

    Update: Added package.use feature allowing you to use local useflags for specific packages

--- install.sh
+++ install.sh
@@ -8,7 +8,9 @@ install -v -m 775 ./Veiler /usr/bin/
 install -v -m 775 -d /var/db/Veiler/
 install -v -m 775 -d /usr/lib/Veiler
 install -v -m 775 -d /var/db/Veiler/local
-tar --strip-components=1 -C /var/db/Veiler/ -xvf db.tar.gz
+tar --strip-components=1 -C /var/db/Veiler/ -xf db.tar.gz
 install -v -m 774 -d /var/cache/Veiler
-install -v -m 775 veiler.conf /etc/
+install -v -m 755 -d /etc/Veiler
+install -v -m 775 veiler.conf /etc/Veiler/
+install -v -m 777 package.use /etc/Veiler/
 set +e
