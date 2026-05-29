216221094887c312f7f726f87b4d88f9d0b4fbad
ilovetrees242 <riyrty594@gmail.com>
2026-05-29 22:52:00 +0530

    Added a build user & useflags

--- install.sh
+++ install.sh
@@ -2,18 +2,16 @@ if [ $(id -u) -ne 0 ]; then
     echo "Run as root."
     exit 1
 fi
-source /etc/os-release
-if [ $ID == "nixos" ]; then
-    echo "NixOS System detected! Please use the derivation file instead."
-    exit 1
-fi
+useradd -M veiler-build-user
 set -e
 trap 'echo "Error while installing"' INT ERR TERM
+chown -R veiler-build-user /var/cache/Veiler
+chmod -R 775 /var/cache/Veiler
 install -v -m 775 ./Veiler /usr/bin/
 install -v -m 775 -d /var/db/Veiler/
 install -v -m 775 -d /var/lib/Veiler
 install -v -m 775 -d /var/db/Veiler/local
-tar --no-same-owner --strip-components=1 -C /var/db/Veiler/ -xf db.tar.gz
+tar --no-same-owner --strip-components=1 -C /var/db/Veiler/ -xf db.tar.xz
 install -v -m 774 -d /var/cache/Veiler
 install -v -m 755 -d /etc/Veiler
 install -v -m 775 veiler.conf /etc/Veiler/
--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,7 @@ if [ $(id -u) -ne 0 ]; then
     echo "Please run as root."
 else
     set -e
+    userdel veiler-build-user
     rm -vf /usr/bin/Veiler
     rm -rvf /var/lib/Veiler
     rm -rfv /var/cache/Veiler /var/db/Veiler
